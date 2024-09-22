e735b8bcab407f74872a9fbb73167b5332479098
Edvin Sidebo <edvin@sidebo.se>
2024-09-22 20:18:46 +0200

    cleanup

--- main.py
+++ main.py
@@ -252,27 +252,9 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                    prog='',
                     description='Round-Robin schedule maker',
-                    epilog='',
-                    #usage='python main.py player1 player2 ...')
                     )
-    #parser.add_argument('players', type=str, nargs='+',
-    #                help='Players in the round-robin')
     args = parser.parse_args()      
-    #players = args.players
-    #if len(players) == 1:
-    #    print(f"Nr of players passed is only 1! Pass at least 2.")
-    #    sys.exit(1)
-    #players_unique = set(players)
-    
-    #if len(players_unique) < len(players):
-    #    print(f"At least one player name was passed multiple time! Give only unique names.")
-    #    sys.exit(1)
-
-    #players = [Player(name=player) for player in players_unique]
-
-    #pprint(round_robin(players))
     main(args)
 
     
